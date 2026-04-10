f7de39a1173da0660f72af9ac1cedbf4bba02d27
Artem Kostylev <a.kostylev@hh.ru>
2026-04-10 10:38:38 +0300

    vite update

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+	plugins: [react()],
+	server: {
+		port: 8080,
+		open: true,
+	},
+	build: {
+		outDir: 'dist',
+	},
+	css: {
+		modules: {
+			generateScopedName: '[name]__[local]__[hash:base64:5]',
+		},
+	},
+});
